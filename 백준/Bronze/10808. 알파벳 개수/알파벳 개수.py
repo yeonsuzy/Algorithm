76affb8c9f8c73b87aa174ee7434deb77d6d8941
Yeon Suzy <88820061+yeonsuzy@users.noreply.github.com>
2024-05-26 22:01:08 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -0,0 +1,4 @@
+s = input()
+cnt = [0]*26
+#a_cnt,b_cnt,c_cnt,d_cnt,e_cnt,f_cnt,g_cnt,h_cnt,i_cnt,j_cnt,k_cnt,l_cnt,m_cnt,n_cnt,o_cnt,p_cnt,q_cnt,r_cnt,s_cnt,t_cnt,u_cnt,v_cnt,w_cnt,x_cnt,y_cnt,z_cnt =0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+print(s.count('a'),s.count('b'),s.count('c'),s.count('d'),s.count('e'),s.count('f'),s.count('g'),s.count('h'),s.count('i'),s.count('j'),s.count('k'),s.count('l'),s.count('m'),s.count('n'),s.count('o'),s.count('p'),s.count('q'),s.count('r'),s.count('s'),s.count('t'),s.count('u'),s.count('v'),s.count('w'),s.count('x'),s.count('y'),s.count('z'),)
