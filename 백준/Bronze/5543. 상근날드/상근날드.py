3018cd7652635585a1e14ad9ef66061457c195d8
Yeon Suzy <88820061+yeonsuzy@users.noreply.github.com>
2024-05-27 22:29:51 +0900

    [Bronze IV] Title: 상근날드, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/5543. 상근날드/상근날드.py
@@ -0,0 +1,14 @@
+arr = []
+brr = []
+
+arr.append(int(input()))
+arr.append(int(input()))
+arr.append(int(input()))
+brr.append(int(input()))
+brr.append(int(input()))
+
+arr.sort()
+brr.sort()
+
+print((arr[0]+brr[0])-50)
+
