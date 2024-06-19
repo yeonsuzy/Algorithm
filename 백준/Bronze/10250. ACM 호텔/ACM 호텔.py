44ede63fa394bcf4bb975ee1fd74bd1e675ab1de
Yeon Suzy <88820061+yeonsuzy@users.noreply.github.com>
2024-06-19 21:10:44 +0900

    [Bronze III] Title: ACM 호텔, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.py
@@ -1,17 +1,9 @@
 t = int(input())
-f, r = 0, 0
 
 for i in range(t):
     h, w, n = map(int, input().split())
 
-    f = n%h
-    r = (n//h)+1
-
-    if f==0:
-        f = h    # 꼭대기층일 경우
-        r = n//h
-
-    if r>=10:
-        print(str(f)+str(r))
-    else:
-        print(str(f)+str(0)+str(r))
+    f = (n-1)%h +1
+    r = (n-1)//h+1
+    
+    print(f*100+r)
